f2dc1a49eb4a6c421f0b5518e4681d9b273c9881
StinkyJeans <johncona3@gmail.com>
2023-10-19 00:42:48 +0800

    added footer component and countrylookup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.svg"/>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        
+        {/*Footer*/}
+
+        <Footer/>
+        
+        </body>
     </html>
   )
 }
