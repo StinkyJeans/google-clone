53242ca106a6225436e69c970d140c22c7983784
StinkyJeans <johncona3@gmail.com>
2023-10-22 01:35:46 +0800

    updated country (united states)

--- src/app/components/CountryLookup.js
+++ src/app/components/CountryLookup.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react'
 
 export default function CountryLookup() {
-    const [country, setCountry] = useState ("")
+    const [country, setCountry] = useState ("United States")
     useEffect(() => {
         fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
         .then((res) => res.json())
