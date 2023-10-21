de0e47620e3d2c22a072a17e3850350c36471d6d
StinkyJeans <johncona3@gmail.com>
2023-10-22 00:57:00 +0800

    add loading effect for web and image pages

--- src/app/components/ImageSearchResults.js
+++ src/app/components/ImageSearchResults.js
@@ -7,7 +7,7 @@ export default function ImageSearchResults({results}) {
 
   return (
     <div className=''>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4 mt-4'>
             {results.items.map((result)=> (
                  <div key={result.link} className='mb-8'>
                     <div className='group'>
